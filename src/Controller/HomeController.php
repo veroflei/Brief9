e494cdf59442e8011dbeb96c0acdfa7d0e733beb
quentin <quentin.destrade@gmail.com>
2023-03-13 11:13:22 +0100

    commit 1
    Ajout fichiers templates

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController{
+
+    /**
+     * @Route("/", name="home")
+     */
+
+     public function index(){
+        return $this->render('Home/accueil.html.twig', [
+            'controllername' => 'HomeController',
+        ]);
+    }
+}
